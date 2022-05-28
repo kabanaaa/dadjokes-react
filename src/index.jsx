8dd0e111a0b7901922bf8262bad7929f8f3f182d
Kateřina Baňacká <banackakaterina@gmail.com>
2022-05-28 17:00:10 +0200

    15 Pokračování vtipu

--- src/index.jsx
+++ src/index.jsx
@@ -1,49 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { useState } from 'react';
 import './style.css';
+import Joke from './Joke';
+import { jokes } from './jokes.js';
 
 const App = () => {
-  const [upLikes, setUpLikes] = useState(0);
-  const [downLikes, setDownLikes] = useState(0);
-
   return (
     <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={() => setUpLikes(upLikes + 1)}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {upLikes}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={() => setDownLikes(downLikes + 1)}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {downLikes}
-          </span>
-        </div>
-      </div>
+      {jokes.map((item) => (
+        <Joke
+          key={item.id}
+          userAvatar={item.avatar}
+          userName={item.name}
+          text={item.text}
+          likes={item.likes}
+          dislikes={item.dislikes}
+        />
+      ))}
     </div>
   );
 };
